e9d0bafd560f27dbada537e4ba8553f08272ab23
Adrien-H <adrien.h@tuta.io>
2019-05-14 17:25:01 +0200

    Add basic navigation

---
+++ components/Navigation.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <nuxt-link to="/" class="navbar-item">/\</nuxt-link>
+    </div>
+
+    <a role="button" class="navbar-burger burger" aria-label="menu"
+       aria-expanded="false" data-target="responsive-navbar"
+    >
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+
+    <div id="responsive-navbar" class="navbar-menu">
+      <div class="navbar-start">
+        <nuxt-link to="/" class="navbar-item">Page d'accueil</nuxt-link>
+        <a href="#" class="navbar-item">Messages privés</a>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">Gestion</a>
+          <div class="navbar-dropdown">
+            <a href="#" class="navbar-item">Troupes</a>
+            <a href="#" class="navbar-item">Ressources</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a href="#" class="button is-light">
+              <strong>Keor</strong>
+            </a>
+            <a href="#" class="button is-dark">
+              Se déconnecter
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </nav>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <navigation></navigation>
     <nuxt/>
   </div>
 </template>
+
+<script>
+  import Navigation from '~/components/Navigation.vue'
+
+  export default {
+    components: {
+      Navigation
+    }
+  }
+</script>
 
 <style lang="scss">
   @import "~bulma";
