dee6d12ae83a27f0580596ad9c6bb86f224c7c72
Adrien-H <adrien.h@tuta.io>
2019-05-17 17:01:28 +0200

    Add transition between pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,12 @@ module.exports = {
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+  /**
+   ** CSS
+   */
+  css: [
+    '~/assets/main.css'
+  ],
   /*
   ** Build configuration
   */
